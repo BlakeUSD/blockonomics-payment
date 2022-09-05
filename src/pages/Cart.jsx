2803eb7345a18d36206bf9c5d4571aaa93457866
Dellano Blake <dellano.blake@outlook.com>
2022-09-05 10:32:14 -0500

    minor adjustments and updates

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import EmptyCart from '../assets/empty_cart.svg'
-// import { Link } from 'react-router-dom'
 
 const Cart = ({ cart, changeQuantity, removeItem }) => {
     const total =() => {
